9db5e5c6840983badd52795779d47b659a7befe2
Nicolette M. Narisma <nicolette.narisma@98labs.com>
2021-08-09 14:16:40 +0800

    added footer under mainFooter file

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,9 @@ import express from 'express';
 const app = express();
 const port = 8000;
 
+app.get('/', (request,response) => {
+    response.send("Hello World!");
+});
+
 app.listen(port);
-console.log("Server is running...");
+console.log("Server is running... ");
